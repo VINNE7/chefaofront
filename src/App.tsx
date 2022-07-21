d477d2d7134aaec5a0899826826af2a7900ff68a
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-07-20 22:56:22 -0300

    add react router dom, phosphor lib, app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import RoutesComponent from './routes'
 import './styles/global.css'
 
 function App() {
 
   return (
     <>
-    <h1 className='text-3xl bg-slate-700 w-fit'>Olá Mundo</h1>
+      <BrowserRouter>
+        <RoutesComponent/>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/routes/index.tsx
@@ -0,0 +1,8 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+
+export default function RoutesComponent(){
+  return(
+    <Routes></Routes>
+  )
+}
