0bb71a48f1d9c0ada7d4a639f833969e6c3755f6
Antonio Emerson <emerson.nessa@gmail.com>
2022-07-21 15:10:04 -0300

    add icones no form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter } from 'react-router-dom'
-import Cadastro from './page/cadastro'
+import Cadastro from './page/Cadastro'
 import RoutesComponent from './routes'
 import './styles/global.css'
 
--- src/page/cadastro.tsx
+++ src/page/cadastro.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 
-
 type FormValues = {
   firstName: string;
   lastName: string;
   email: string;
+  password: string;
 };
 
 export default function Cadastro() {
@@ -16,18 +16,23 @@ export default function Cadastro() {
   return (
     <>
       <div className="min-h-screen w-screen flex justify-center items-center flex-col  bg-slate-800">
-        <h1 className="text-white text-4xl text-center font-bold">Cadastro de Usuário</h1>
+        <h1 className="text-white text-4xl text-center font-bold">
+          Cadastro de Usuário
+        </h1>
         <form
           onSubmit={handleSubmit(onSubmit)}
           className="w-4/5 m-0 bg-slate-800 p-3 "
         >
-          <div className="">
+          <div className="input-field">
             <label className="leading-3 text-left block mb-3 mt-5 text-white text-sm font-extralight">
               Nome Completo
             </label>
+            <i className="fa fa-user icon absolute w-full mb-2 p-2 min-w-5"/>
             <input
+
               className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
-              {...register("firstName")}/>
+              {...register("firstName")}
+            />
           </div>
 
           <div>
@@ -37,6 +42,7 @@ export default function Cadastro() {
             >
               E-mail
             </label>
+            <i className="fa fa-envelope icon absolute w-full mb-2 p-2 min-w-5"/>
             <input
               className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
               {...register("lastName")}
@@ -50,6 +56,7 @@ export default function Cadastro() {
             >
               Senha
             </label>
+            <i className="fa fa-key icon absolute w-full mb-2 p-2 min-w-5"/>
             <input
               className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
               type="email"
@@ -57,18 +64,22 @@ export default function Cadastro() {
             />
           </div>
 
-          <div>
+          <div className="input-field">
             <label
               className="leading-3 text-left block mb-3 mt-5 text-white text-sm font-extralight"
               htmlFor="password"
             >
               Confirmar Senha
             </label>
+            <i className="fa fa-key icon absolute w-full mb-2 p-2 min-w-5"/>
+
             <input
-              className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-8 text-sm"
+              className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-8 text-sm "
               type="email"
               {...register("email")}
+
             />
+
           </div>
         </form>
         <input
