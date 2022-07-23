019e9b57419a7f3ad70ed5d3c4e18fb54243edd7
Leonardo Barreiro <leobcss@gmail.com>
2022-07-23 16:35:20 -0300

    cria paleta de cores no arquivo tailwind.config.cjs, corrige caminho de importação no arquivo de rotas

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        semiwhite: "#fafafa",
+        ghostwhite: "#f0f0f5",
+        blueywhite: "#e8edfd",
+        raisinblack: "#24252E",
+        oxfordblue: "#0B1B34",
+        royalblue: "#001B42",
+        yellowcrayolla: "#FFE75C",
+        cyberyellow: "#FFD400",
+        darkgolden: "#E9BA1D",
+      },
+    },
   },
   plugins: [],
 };
