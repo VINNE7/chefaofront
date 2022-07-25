46d2bbf51d1b825de13d5fffca84e60092c3c9fb
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-25 15:56:06 -0300

    Estilizando a página e refatorando uns códigos

--- src/pages/firstStablishmentSignup/index.tsx
+++ src/pages/firstStablishmentSignup/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import FormButton from "../../components/button";
+import { States, Types } from "./Object";
 
 type FormValues = {
   empresa: string;
@@ -35,49 +36,54 @@ export default function FirstStablishmentSignup() {
       });
   };
 
+
   return (
-    <div className="min-h-screen w-screen flex justify-center items-center flex-col  bg-slate-800">
-      <h1 className=" text-white text-2xl text-center font-bold">
-        Dados do Estabelecimento
-      </h1>
+    <div className="min-h-screen w-screen flex justify-center items-center flex-col  bg-white">
+      <div className="">
+        <h1 className=" text-[#000000] text-2xl text-center font-bold ">
+          Dados do Estabelecimento
+        </h1>
+      </div>
       <form
         onSubmit={handleSubmit(onSubmit)}
-        className="w-4/5 m-0 bg-slate-800 p-3 "
+        className="w-4/5 m-0  p-3 "
       >
         <div className="">
-          <label className="leading-3 text-left block mb-3 mt-5 text-white text-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold  ">
             Nome da Empresa
           </label>
           <input
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm "
             placeholder="Digite o nome do estabelecimento"
             type="text"
             {...register("empresa", { required: true })}
           />
         </div>
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
             Segmento
           </label>
 
           <select
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             required
             {...register("segmento", { required: true })}
           >
-            <option value="disable">Selecione um Segmento</option>
-            <option value="pizzaria">Pizzaria</option>
-            <option value="lanchonete">Lanchonente</option>
-            <option value="comida boteco">Comida de Buteco</option>
+            {Types.map(
+              (type) =>(
+                <option value={type.value}>{type.type}</option>
+              )
+            )}
+           
           </select>
         </div>
 
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
             CEP
           </label>
           <input
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             placeholder="Digite o CEP do estabelecimento"
             id="cep"
             type="text"
@@ -87,62 +93,40 @@ export default function FirstStablishmentSignup() {
         </div>
 
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
             Estado
           </label>
 
           <select
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             required
             {...register("estado")}
+          
           >
-            <option value="disable">Selecione um estado</option>
-            <option value="AC">Acre</option>
-            <option value="AL">Alagoas</option>
-            <option value="AP">Amapá</option>
-            <option value="AM">Amazonas</option>
-            <option value="BA">Bahia</option>
-            <option value="CE">Ceará</option>
-            <option value="DF">Distrito Federal</option>
-            <option value="ES">Espirito Santo</option>
-            <option value="GO">Goiás</option>
-            <option value="MA">Maranhão</option>
-            <option value="MT">Mato Grosso</option>
-            <option value="MS">Mato Grosso do Sul</option>
-            <option value="MG">Minas Gerais</option>
-            <option value="PA">Pará</option>
-            <option value="PB">Paraíba</option>
-            <option value="PR">Paraná</option>
-            <option value="PE">Pernanbumco</option>
-            <option value="PI">Piauí</option>
-            <option value="RJ">Rio de Janeiro</option>
-            <option value="RN">Rio Grande do Norte</option>
-            <option value="RS">Rio Grande do Sul</option>
-            <option value="RO">Rondônia</option>
-            <option value="RR">Roraima</option>
-            <option value="SC">Santa Catarina</option>
-            <option value="SP">São Paulo</option>
-            <option value="SE">Sergipe</option>
-            <option value="TO">Tocantins</option>
+            {States.map( 
+              (state) => (
+                <option value={state.value}>{state.state}</option>
+              )
+            )}
           </select>
         </div>
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
             Cidade
           </label>
           <input
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             placeholder="Digite o nome da cidade"
             required
             {...register("cidade")}
           />
         </div>
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5  text-[#24252E] text-base font-bold">
             Endereço da empresa
           </label>
           <input
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             placeholder="Digite o nome da rua"
             required
             {...register("endereco")}
@@ -150,23 +134,23 @@ export default function FirstStablishmentSignup() {
         </div>
         <div className="flex gap-10">
           <div className="flex flex-col">
-            <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+            <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
               Numero
             </label>
 
             <input
-              className=" box-border w-28 rounded border-solid border-1 border-white py-2 px-1 mb-3 text-sm"
+              className=" box-border w-28 rounded border-solid border border-[#001B42] py-2 px-1 mb-3 text-sm"
               placeholder="Digite o número"
               required
               {...register("numero")}
             />
           </div>
           <div className="flex flex-col">
-            <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+            <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
               Bairro
             </label>
             <input
-              className=" box-border w-28 rounded border-solid border-1 border-white py-2 px-2 mb-3 text-sm"
+              className=" box-border w-28 rounded border-solid border border-[#001B42] py-2 px-2 mb-3 text-sm"
               placeholder="Digite o bairro"
               required
               {...register("bairro")}
@@ -174,11 +158,11 @@ export default function FirstStablishmentSignup() {
           </div>
         </div>
         <div>
-          <label className="leading-3 text-left block mb-3 mt-5 text-white tex-sm font-extralight">
+          <label className="leading-3 text-left block mb-3 mt-5 text-[#24252E] text-base font-bold">
             Contato ou whatsapp
           </label>
           <input
-            className="block box-border w-full rounded border-solid border-1 border-white py-2 px-4 mb-3 text-sm"
+            className="block box-border w-full rounded border-solid border border-[#001B42] py-2 px-4 mb-3 text-sm"
             placeholder="Digite o nome da cidade"
             required
             {...register("contato")}
