a741e7067ee3c4c56975fdcfd5050b6c257f891f
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-07-25 20:19:11 -0300

    página de estabelecimento não utilizavel

--- src/pages/secondStablishmentSignup/index.tsx
+++ src/pages/secondStablishmentSignup/index.tsx
@@ -1,29 +1,81 @@
+import DateContainer from "../../components/DateRow";
+import DateRow from "../../components/DateRow";
+import EditIcon from "../../components/icons/editIcon";
+import Modal from 'react-modal';
+import { useState } from "react";
+import ReactModal from "react-modal";
+import { DataCheckbox } from "./data";
+
+Modal.setAppElement('#root')
+
+
+
+
+
 export default function SecondStablishmentSignup(){
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal(){
+    setIsOpen(true);
+  }
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+
   return(
-    <div className="flex flex-col w-screen h-screen bg-slate-900">
-      <div className="flex flex-col mx-auto gap-4">
-      <h1 className="text-white text-4xl font-bold">Funcionamento</h1>
+    <div className="w-screen h-screen bg-semiwhite font-tahoma">
 
-      <form action="" className="text-white mt-10">
-        <div className="input-field flex flex-col">
-          <label htmlFor="">Contato Whatsapp</label>
-          <input type="text" placeholder="(XX) X XXXX-XXXX" className="h-10 p-2 text-black" />
-        </div>
-      </form>
+      <div className="flex flex-col py-10 max-w-xs mx-auto">
+        <h1 className="text-2xl font-bold text-center mb-10">Horário de Funcionamento</h1>
+
+      <div className="mb-8">
+        <DateRow dayText="Segunda"/>
+        <DateRow dayText="Terça"/>
+        <DateRow dayText="Quarta"/>
+        <DateRow dayText="Quinta"/>
+        <DateRow dayText="Sexta"/>
+        <DateRow dayText="Sábado"/>
+        <DateRow dayText="Domingo"/>
       </div>
 
-      <div className="flex flex-col mt-10 items-center text-white text-lg">
-        <h2 className="my-2">Dias/horários de funcionamento</h2>
-        <div className="w-[calc(100%-20px)] p-2 border border-white flex justify-end  gap-2">
-          <button className=" flex gap-2 items-center"> <i className="fa fa-plus"></i> Adicionar</button>
-        </div>
-        <ul className="w-[calc(100%-20px)] h-80 border border-white">
-          <li className="flex justify-between m-4 border-b border-white" tabIndex={0}>
-            <span>SEG</span>
-            <div className="flex gap-8 items-center"><span>8:00</span> <span>12:00</span> <button><i className="fa fa-trash"></i></button></div>
-          </li>
-        </ul>
+      <button className="text-sm text-royalblue font-bold border-b border-royalblue self-center mb-8" onClick={openModal} >Editar Todos os Horários</button>
+
+      <div className="flex items-center justify-center gap-5 mx-8"> 
+        <button className=" flex-1 border-2 rounded-2xl border-cyberyellow  py-2 hover:border-darkgolden hover:bg-yellowcrayolla transition-colors">Voltar</button>
+        <button className=" flex-1 border-2 rounded-2xl border-ghostwhite bg-ghostwhite text-grayDisabled py-2 ">Salvar</button>
       </div>
+
+      <ReactModal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Caixa para edição de horário de funcionamento"
+        overlayClassName="modal-overlay"
+        className="modal-content  w-[316px] h-[600px] bg-white rounded-lg"
+      >
+          <div className="flex flex-col bg-white pt-4 pb-6 rounded-lg ">
+            <h1 className="font-bold text-black text-base text-center  ">Selecione os dias e horários</h1>
+            <form action="">
+
+              <div className="flex px-2  justify-between ">
+                {DataCheckbox.map(({name, label}, index)=>(
+                  <label className="relative" key={index}>
+                    <input type="checkbox" className="appearance-none absolute inset-0 cursor-pointer peer" name={name}  />
+                    <span className="text-grayDisabled border rounded-md justify-center text-sm peer-checked:bg-raisinblack peer-checked:text-cyberyellow px-[6px] py-0.5">{label}</span>
+                  </label>
+                ))}       
+              </div>
+
+              <div>
+                
+              </div>
+ 
+            </form>
+          </div>
+      </ReactModal>
+
+      </div>
+
     </div>
   )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,9 +12,15 @@ module.exports = {
         yellowcrayolla: "#FFE75C",
         cyberyellow: "#FFD400",
         darkgolden: "#E9BA1D",
+        error: "#DA1414",
+        colorFocus: "#2E5AAC",
+        grayDisabled: "#A3A3A3"
       },
       fontFamily: {
         tahoma: 'Tahoma'
+      },
+      backgroundImage: {
+        'radioChecked': "url('/src/assets/images/radioChecked.svg')"
       }
     },
   },
