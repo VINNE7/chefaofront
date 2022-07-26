997c474b43d5cdaa27774e73e9f00e2635b1a3df
Leonardo Barreiro <leobcss@gmail.com>
2022-07-26 00:40:20 -0300

    altera identação

--- src/components/buttonBack/index.tsx
+++ src/components/buttonBack/index.tsx
@@ -1,13 +1,12 @@
 interface IButtonProps {
-  buttonLabel : string;
-  type?: string
+  buttonLabel: string;
+  type?: string;
 }
 
-export function FormButtonBack(props: IButtonProps){
-  return(
-    <button
-     className="box-border border border-cyberyellow py-2 px-4 rounded-[10px] w-[146px]  items-center justify-center text-center ">
+export function FormButtonBack(props: IButtonProps) {
+  return (
+    <button className="box-border border border-cyberyellow py-2 px-4 rounded-[10px] w-[146px]  items-center justify-center text-center ">
       {props.buttonLabel}
     </button>
-  )
+  );
 }
