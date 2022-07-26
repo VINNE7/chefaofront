43c64aec0066a9601699ad2f00cbd5870c69745c
Antonio Emerson <emerson.nessa@gmail.com>
2022-07-25 22:35:35 -0300

    ajuste pagina home

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -6,7 +6,6 @@ interface IButtonProps {
 export default function FormButton(props: IButtonProps){
   return(
     <button
-      type={props.type}
      className="box-border rounded-lg bg-[#FFD400] py-2 px-4 text-base text-[#24252E] font-bold w-1/4 items-center justify-center text-center">
       {props.buttonLabel}
     </button>
