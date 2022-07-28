f61a1cdb43a37c8f9986bba12f666a6b10319d28
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-28 20:12:16 -0300

    Add form no Category

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,6 +1,17 @@
+import {useState} from "react";
+
 export default function Category(){
+    const [inputTable, setinputTable] = useState("Nome da Categoria")
+    const [isEditable, setIsEditable] = useState(false)
     return (
         <>
+            <form action="">
+                <input type="text" 
+                disabled={isEditable === false}
+                value={inputTable}
+                onChange={(e:any)=>setinputTable(e.target.value)}
+            />
+            </form>
         </>
     )
 }
