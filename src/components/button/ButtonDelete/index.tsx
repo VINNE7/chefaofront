d993a2a5cc1e502c0a7ff6cd2ce3ce804339c847
Antonio Emerson <emerson.nessa@gmail.com>
2022-07-29 14:00:37 -0300

    adicionado botão de excluir com modal

---
+++ src/components/button/ButtonDelete/index.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import imgTrash from "../../../assets/icons/trash.png";
+
+export function ButtonDelete() {
+  const [showModal, setShowModal] = useState(false);
+
+  return (
+    <>
+      <button className="w-full h-[48px] flex flex-row justify-between items-center">
+        <h2 className="text-raisinblack text-xl font-bold my-10">
+          Excluir Categoria
+        </h2>
+        <img
+          className="w-[20px] h-[24px]"
+          src={imgTrash}
+          alt="icone para deletar"
+          onClick={() => setShowModal(true)}
+        />
+
+        {showModal ? (
+          <>
+            <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+              <div className="relative w-[314px] h-[152px] my-6 mx-auto max-w-sm">
+                <div className="border-0 rounded-[12px] shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  <div className="relative mx-6 flex-auto">
+                    <p className="mt-6 mb-12 text-raisinblack text-[16px] font-bold">
+                      Tem certeza que deseja excluir?
+                    </p>
+                  </div>
+                  <div className="flex justify-evenly mb-5">
+                    <button
+                      className="formButtonBackMin font-bold text-[16px]"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Cancelar
+                    </button>
+                    <button
+                      className="formButtonForwardMax font-bold text-[16px]"
+                      type="button"
+                      onClick={() => setShowModal(true)}
+                    >
+                      Excluir
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="opacity-50 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+      </button>
+    </>
+  );
+}
--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -1,7 +1,7 @@
-import { Category } from "../../components/Category";
 import MenuHeader from "../../components/MenuHeader";
 import imgPen from "../../assets/icons/pen.png";
 import { SubCategory } from "../../components/Subcategory";
+import { ButtonDelete } from "../../components/button/ButtonDelete";
 
 export default function MenuPage() {
   return (
@@ -10,24 +10,24 @@ export default function MenuPage() {
 
       <div className="mx-8 ">
         <h2 className="text-raisinblack text-xl font-bold my-4 ">Categorias</h2>
-        <div>
-          
-        </div>
+
         <div className="flex justify-between items-center">
           <h2 className="text-raisinblack text-xl font-bold my-10">
             Nome da Categoria
           </h2>
           <button>
-          <img
-            className="w-[21px] h-[18px]"
-            src={imgPen}
-            alt="icone para modificar pedido"
-          />
+            <img
+              className="w-[21px] h-[18px]"
+              src={imgPen}
+              alt="icone para modificar pedido"
+            />
           </button>
-
         </div>
         <div className="flex justify-center itemx-center ">
           <SubCategory />
+        </div>
+        <div className="my-10">
+          <ButtonDelete />
         </div>
       </div>
     </div>
