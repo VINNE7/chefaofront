e70809b2746d97f7ffd57e48ea80712a2c3b0868
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-29 19:29:14 -0300

    Arrumando o input

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -65,7 +65,7 @@ export default function Category(){
                     </div>
                 ))}
                 <button 
-                    onClick={()=> setSubcards(state => ([...state,"Nova categoria"]))}
+                    onClick={()=> setSubcards(state => ([...state,"Nova Subcategoria"]))}
                     className="bg-[#001B42] shadow-md flex w-80 rounded-lg p-4 gap-1 items-center object-cover ">
                     <span className="bg-[#001B42] shadow-md text-[#FAFAFA]">
                     Adicionar subcategoria{" "}
