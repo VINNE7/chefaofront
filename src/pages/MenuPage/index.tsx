8bcae7d8d8e9565a97f135a9ebdbaf53a3248e19
Leonardo Barreiro <leobcss@gmail.com>
2022-07-29 18:28:52 -0300

    importa o componente Item e o inclui na Page

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -1,10 +1,11 @@
-export default function MenuPage (){
-  return(
+import Item from "../../components/Item";
+
+export default function MenuPage() {
+  return (
     <div className="flex w-[316px] mx-auto">
       <div>
         <h1 className="text-raisinblack text-2xl font-bold">Categorias</h1>
-
-        
+        <Item name="Linguiça" price="47,99"></Item>
       </div>
     </div>
   );
