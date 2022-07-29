318bdcf53f2e0756d84de14a5740cb0aa4b5afff
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-07-28 23:19:49 -0300

    Criando botão add category

--- src/components/AddCategory/index.tsx
+++ src/components/AddCategory/index.tsx
@@ -1,12 +1,27 @@
-import Plus from "../../assets/icons/Vector.png"
+import Plus from "../../assets/icons/Vector.png";
+import { useState } from "react";
 
-export default function AddCategory(){
-    return(
-        <>
-            <button className="bg-[#001B42] shadow-md flex w-36 rounded-lg p-4 gap-1 items-center object-cover ">
-                <span className="bg-[#001B42] shadow-md text-[#FAFAFA]">Adicionar Categorias </span>
-                <img className="" src={Plus} alt="" />
-            </button>
-        </>
-    )
+export default function AddCategory() {
+  function addCateg() {
+    setaddSubcategory([...addSubcategory, addSubcategory]);
+  }
+
+  const [addSubcategory, setaddSubcategory] = useState([{}]);
+
+  return (
+    <>
+      <button
+        onClick={addCateg}
+        className="bg-[#001B42] shadow-md flex w-36 rounded-lg p-4 gap-1 items-center object-cover "
+      >
+        {addSubcategory.map((category) => (
+          <p>Teste</p>
+        ))}
+        <span className="bg-[#001B42] shadow-md text-[#FAFAFA]">
+          Adicionar Categorias{" "}
+        </span>
+        <img className="" src={Plus} alt="" />
+      </button>
+    </>
+  );
 }
