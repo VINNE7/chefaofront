227b47739ac8cb227321823357b85905426d5da4
Marcus Vinicius <marcusv.ribas7@gmail.com>
2022-08-04 19:01:57 -0300

    changed category and subcategory cards for Joao Antonio

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -3,15 +3,23 @@ import Editsub from "../../assets/icons/editsub.png";
 import Drop from "../../assets/icons/drop.png";
 import Plus from "../../assets/icons/Vector.png";
 import { useState } from "react";
+import AddCardIcon from "../icons/editIcon/addCardIcon";
 
 interface Newcard{
   id: number
   title: string
 }
 
+
+interface ICategoryCard{
+  name: string
+  id: number
+}
+
 export default function Category() {
-  const [cards, setCards] = useState(["Pizza"]);
-  const [subcards, setSubcards] = useState([]);
+  const [categories, setCategories] = useState<ICategoryCard[]>([{name: "Pizza", id: 1}]);
+  const [selectedCategoryNumber, setSelectedCategoryNumber] = useState(1);
+  const [subCategories, setSubCategories] = useState([{categoryId: selectedCategoryNumber, name: "Nova subcategoria", items: [{name:"moda da casa", image: [], price: 0}]  }]);
   const [isEditable, setIsEditable] = useState(false);
   const [editable, setEditable] = useState(false);
   const [establishmentName, setEstablishmentName] =
@@ -20,6 +28,8 @@ export default function Category() {
     useState([{title: "Nova Subcategoria", id: Math.random()}]);
   const [establishmentSubcategoryTitle, setestablishmentSubcategoryTitle] =
     useState("Nome da Subcategoria");
+
+  
 
 
   function createSubcategorycard(){
@@ -39,24 +49,24 @@ export default function Category() {
   
   return (
     <>
-      <div className="text-[#24252E] text-xl font-bold p-4">
+      <div className="text-[#24252E] text-xl  font-bold p-4">
         <h3>Categorias</h3>
       </div>
       <div className="flex  gap-4 overflow-x-auto scroll-smooth  p-4">
-        {cards.map((card) => (
+        {categories.map((card) => (
             <button 
-              className="font-bold text-base leading-6 bg-cyberyellow p-[30px]  rounded-lg shadow-md w-full max-w-[160px] flex justify-center">
-              {card}
+              onClick={()=>setSelectedCategoryNumber(card.id)}
+              key={card.id}
+              className="font-bold  px-12 py-4 text-base leading-6 text-white bg-royalblue  rounded-lg shadow-md  flex justify-center items-center focus:bg-cyberyellow focus:text-royalblue">
+              {card.name} 
             </button>
         ))}
         <button
-          onClick={() => setCards((state) => [...state, "Nova categoria"])}
-          className="bg-[#001B42] shadow-md flex w-full max-w-[160px] rounded-lg p-[30px] gap-1 items-center object-cover "
+          onClick={() => setCategories((state) => [...state, {name: "Nova Categoria", id: state.length + 1 } ])}
+          className="bg-royalblue shadow-md  rounded-lg p-4 flex items-center text-white font-bold  "
         >
-          <span className="bg-[#001B42] shadow-md text-[#FAFAFA]">
-            Adicionar Categorias{" "}
-          </span>
-          <img className="" src={Plus} alt="" />
+            Adicionar Categorias
+          <AddCardIcon/>
         </button>
       </div>
       <div className="flex  justify-center ">
@@ -72,7 +82,34 @@ export default function Category() {
         </button>
       </div>
       <div className="pl-4">
-        {subcards.map((subcard: Newcard ) => (
+
+        {subCategories.map((subCategory, i)=>
+          subCategory.categoryId == selectedCategoryNumber ? (
+          <div className="bg-oxfordblue p-6 justify-between  rounded-lg shadow-md w-80 flex text-center mb-1 ">
+            <div className="text-left flex gap-4 ">
+              <input
+                className="bg-oxfordblue font-bold w-[150px] text-base   leading-6 text-semiwhite"
+                type="text"
+                disabled={editable === false}
+                value={subCategory.name}
+                onChange={(e: any) => setestablishmentSubcategory(e.target.value)}
+              />
+              <button >
+                <img className="" src={Editsub} alt="teste" />
+              </button>
+            </div>
+            <div className="flex">
+              <button>
+                <img className="" src={Drop} alt="" />
+              </button>
+            </div>
+          </div>
+          ) : (
+            <></>
+          ) 
+        )}
+
+        {/* {subcards.map((subcard: Newcard ) => (
           <div className="bg-oxfordblue p-6 justify-between  rounded-lg shadow-md w-80 flex text-center mb-1 ">
             <div className="text-left ">
               <input
@@ -92,7 +129,7 @@ export default function Category() {
               </button>
             </div>
           </div>
-        ))}
+        ))} */}
         <button
           onClick={ () => createSubcategorycard()}
           className="bg-[#001B42] shadow-md flex w-80 rounded-lg p-4 gap-1 items-center object-cover mt-6 "
---
+++ src/components/icons/editIcon/addCardIcon/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function AddCardIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="41"
+      height="41"
+      fill="none"
+      viewBox="0 0 25 25"
+    >
+      <path
+        fill="#FFD400"
+        d="M12.5 0A12.654 12.654 0 000 12.5 12.654 12.654 0 0012.5 25 12.653 12.653 0 0025 12.5 12.654 12.654 0 0012.5 0zm7.143 13.393h-6.25v6.25h-1.786v-6.25h-6.25v-1.786h6.25v-6.25h1.786v6.25h6.25v1.786z"
+      ></path>
+    </svg>
+  );
+}
+
+export default AddCardIcon;
