789cefe3fcbdb5af3f48e3a149c67f28bc91064b
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-08-04 23:52:56 -0300

    Padronizando o codigo

--- src/pages/MenuPage/index.tsx
+++ src/pages/MenuPage/index.tsx
@@ -1,7 +1,4 @@
 import MenuHeader from "../../components/MenuHeader";
-import Item from "../../components/Item";
-import imgPen from "../../assets/icons/pen.png";
-import { SubCategory } from "../../components/Subcategory";
 import Category from "../../components/Category";
 
 export default function MenuPage() {
@@ -9,25 +6,7 @@ export default function MenuPage() {
     <div className="grid gap-4 ">
       <MenuHeader />
       <div className="mx-8 ">
-        <h2 className="text-raisinblack text-xl font-bold my-4 ">Categorias</h2>
         <Category />
-        <div className="flex justify-between items-center">
-          <h2 className="text-raisinblack text-xl font-bold my-10">
-            Nome da Categoria
-          </h2>
-          <button>
-            <img
-              className="w-[21px] h-[18px]"
-              src={imgPen}
-              alt="icone para modificar pedido"
-            />
-          </button>
-        </div>
-        <Item name="" price="" />
-        <div className="flex justify-center itemx-center ">
-          <SubCategory />
-        </div>
-        <div className="my-10"></div>
       </div>
     </div>
   );
